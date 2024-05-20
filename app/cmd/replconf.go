09ff0fa5541f747e5fb0c8d6b960fca855503b22
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2024-05-20 19:03:22 +0530

    codecrafters submit [skip ci]

--- app/cmd/replconf.go
+++ app/cmd/replconf.go
@@ -8,14 +8,44 @@ import (
 )
 
 func ReplConf(conn net.Conn, args []string, serverState *types.ServerState) {
-	bytes, err := respHandler.Str.Encode("OK")
-	if err != nil {
-		fmt.Println("Failed to encode response", err)
-		return
-	}
-	conn.Write(bytes)
-
+	// If the command is REPLCONF listening-port, add the connection to the list of replica connections
 	if len(args) >= 2 && args[0] == "listening-port" {
 		serverState.ReplicaConn = append(serverState.ReplicaConn, &conn)
+		sendOk(conn)
+		return
+	}
+
+	// If the command is REPLCONF capa, send OK
+	if len(args) >= 2 && args[0] == "capa" {
+		sendOk(conn)
+		return
+	}
+
+	// If the command is REPLCONF GETACK *, send an ACK back to the replica
+	if len(args) >= 2 && args[0] == "GETACK" && args[1] == "*" {
+		getAck(conn)
+		return
+	}
+
+	fmt.Printf("Unknown REPLCONF command: %s\n", args)
+}
+
+func getAck(conn net.Conn) {
+	bytes := respHandler.Array.Encode([]string{"REPLCONF", "ACK", fmt.Sprintf("%d", 0)})
+	_, err := conn.Write(bytes)
+	if err != nil {
+		fmt.Println("Failed to write response ACK response to master: ", err)
+	}
+}
+
+func sendOk(conn net.Conn) {
+	bytes, err := respHandler.Str.Encode("OK")
+	if err != nil {
+		fmt.Println("Failed to encode OK response: ", err)
+		return
+	}
+	_, err = conn.Write(bytes)
+	if err != nil {
+		fmt.Println("Failed to write OK response to master: ", err)
 	}
 }
