2f36bac2a2343253dcbc39b5f4f4c5ddb589d2c7
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2024-05-22 14:54:38 +0530

    codecrafters submit [skip ci]

--- app/cmd/xadd.go
+++ app/cmd/xadd.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/codecrafters-io/redis-starter-go/app/types"
 )
@@ -63,7 +64,7 @@ func Xadd(conn net.Conn, server *types.ServerState, args ...string) {
 	})
 
 	// Return the ID of the added item
-	res, err := respHandler.Str.Encode(itemKey)
+	res, err := respHandler.Str.Encode(validatedEntryId)
 	if err != nil {
 		fmt.Printf("Error encoding response: %s\n", err)
 		return
@@ -76,32 +77,10 @@ func Xadd(conn net.Conn, server *types.ServerState, args ...string) {
 
 // getValidatedEntryID parses the provided entry ID and returns the validated entry id and an error string if any
 func getValidatedEntryID(entryID string, lastEntryID *string) (string, string) {
-	entryParts := strings.Split(entryID, "-")
-	if len(entryParts) != 2 {
-		return "", "ERR The provided entry should have atleast two parts separated by a -"
-	}
-
-	// Parse the timestamp and sequence number
-	timeStamp, err := strconv.Atoi(entryParts[0])
-	if err != nil {
-		return "", fmt.Sprintf("ERR Can't parse valid timestamp from entry ID: %v", err)
-	}
-	seqNumber, err := strconv.Atoi(entryParts[1])
-	if err != nil {
-		return "", fmt.Sprintf("ERR Can't parse valid sequence number from entry ID: %v", err)
-	}
-
-	// Minimum value of the ID check
-	if timeStamp == 0 && seqNumber == 0 {
-		return "", "ERR The ID specified in XADD must be greater than 0-0"
-	}
-
-	// If the last entry is nill, then the entry is valid
 	if lastEntryID == nil {
-		return entryID, ""
+		return handleNoLastEntry(entryID)
 	}
 
-	// Parse the last entry ID
 	lastEntryParts := strings.Split(*lastEntryID, "-")
 	lastTimeStamp, err := strconv.Atoi(lastEntryParts[0])
 	if err != nil {
@@ -112,10 +91,92 @@ func getValidatedEntryID(entryID string, lastEntryID *string) (string, string) {
 		panic("The last entry ID is not in the correct format")
 	}
 
-	// Check that the entry should be greater than the last entry
-	if timeStamp > lastTimeStamp || (timeStamp == lastTimeStamp && seqNumber > lastSeqNumber) {
-		return entryID, ""
+	return handleWithLastEntry(entryID, int64(lastTimeStamp), lastSeqNumber)
+}
+
+// Provides the validated entry ID when there is no last entry
+func handleNoLastEntry(entryID string) (string, string) {
+	// Generate the new entry ID completely
+	if entryID == "*" {
+		return fmt.Sprintf("%d-0", time.Now().UnixMilli()), ""
 	}
 
-	return "", "ERR The ID specified in XADD is equal or smaller than the target stream top item"
+	entryParts := strings.Split(entryID, "-")
+	if len(entryParts) != 2 {
+		return "", "ERR The provided entry should have atleast two parts separated by a -"
+	}
+
+	// Use the provided timestamp and use 0 as the sequence number
+	if entryParts[1] == "*" {
+		var seqID int
+		if entryParts[0] == "0" {
+			seqID = 1
+		} else {
+			seqID = 0
+		}
+		return fmt.Sprintf("%s-%d", entryParts[0], seqID), ""
+	}
+
+	// Handle the only invalid case
+	if entryParts[0] == "0" && entryParts[1] == "0" {
+		return "", "ERR The ID specified in XADD must be greater than 0-0"
+	}
+
+	return entryID, ""
+}
+
+// Provides the validated entry ID when there is a last entry
+func handleWithLastEntry(entryID string, lastTimestamp int64, lastSeqNo int) (string, string) {
+	// Generate the new entry ID completely
+	if entryID == "*" {
+		currTimestamp := time.Now().UnixMilli()
+		curSeqNo := 0
+		if currTimestamp < lastTimestamp {
+			return "", "ERR The timestamp part of the entry ID is smaller than the last entry's timestamp"
+		}
+		if currTimestamp == lastTimestamp {
+			curSeqNo = lastSeqNo + 1
+		}
+		return fmt.Sprintf("%d-%d", currTimestamp, curSeqNo), ""
+	}
+
+	entryParts := strings.Split(entryID, "-")
+	if len(entryParts) != 2 {
+		return "", "ERR The provided entry should have atleast two parts separated by a -"
+	}
+
+	// Generate the sequence number based on the last entry
+	if entryParts[1] == "*" {
+		parsedTime, err := strconv.Atoi(entryParts[0])
+		if err != nil {
+			return "", "ERR The timestamp part of the entry ID is not a valid integer"
+		}
+		currTimestamp := int64(parsedTime)
+		if currTimestamp < lastTimestamp {
+			return "", "ERR The timestamp part of the entry ID is smaller than the last entry's timestamp"
+		}
+		if currTimestamp == lastTimestamp {
+			return fmt.Sprintf("%d-%d", currTimestamp, lastSeqNo+1), ""
+		}
+		return fmt.Sprintf("%d-0", currTimestamp), ""
+	}
+
+	// Handle the case when both are provided
+	entryTimestamp, err := strconv.Atoi(entryParts[0])
+	if err != nil {
+		return "", "ERR The timestamp part of the entry ID is not a valid integer"
+	}
+	entrySeqNo, err := strconv.Atoi(entryParts[1])
+	if err != nil {
+		return "", "ERR The sequence number part of the entry ID is not a valid integer"
+	}
+
+	if entryTimestamp == 0 && entrySeqNo == 0 {
+		return "", "ERR The ID specified in XADD must be greater than 0-0"
+	}
+	if entryTimestamp < int(lastTimestamp) || (entryTimestamp == int(lastTimestamp) && entrySeqNo <= lastSeqNo) {
+		return "", "ERR The ID specified in XADD is equal or smaller than the target stream top item"
+	}
+
+	return entryID, ""
 }
