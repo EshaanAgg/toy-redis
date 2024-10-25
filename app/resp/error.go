ec5ad0f5582a0b2c3a8acff3f8a00684a3568623
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2024-10-25 22:05:23 +0530

    Add changes to client

--- app/resp/error.go
+++ app/resp/error.go
@@ -1,7 +1,22 @@
 package resp
 
+import "fmt"
+
 type errorValue struct{}
 
 func (errorValue) Encode(err string) []byte {
 	return []byte("-" + err + "\r\n")
+}
+
+func (errorValue) Decode(b []byte) (string, error) {
+	if b[0] != '-' {
+		return "", fmt.Errorf("not a correct error value, should begin with -, but does with %b", b[0])
+	}
+
+	l := len(b)
+	if string(b[l-2:]) != "\r\n" {
+		return "", fmt.Errorf("error value must end in CRLF, but it ends in %s", string(b[l-2:]))
+	}
+
+	return string(b[1 : l-2]), nil
 }
--- app/resp/integer.go
+++ app/resp/integer.go
@@ -1,10 +1,31 @@
 package resp
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type integer struct{}
 
-func (i integer) Encode(n int) []byte {
+func (integer) Encode(n int) []byte {
 	message := fmt.Sprintf(":%d\r\n", n)
 	return []byte(message)
+}
+
+func (integer) Decode(b []byte) (int, error) {
+	if b[0] != ':' {
+		return 0, fmt.Errorf("not correct encoding for int, should begin with :, but does with %b", b[0])
+	}
+
+	l := len(b)
+	if string(b[l-2:]) != "\r\n" {
+		return 0, fmt.Errorf("error value must end in CRLF, but it ends in %s", string(b[l-2:]))
+	}
+
+	n, err := strconv.Atoi(string(b[1 : l-2]))
+	if err != nil {
+		return 0, fmt.Errorf("can't convert the passed bytes to integer: %v", err)
+	}
+
+	return n, err
 }
